0d5e51feb2677fe2447bbdd4a5f3b939b457858d
Aidan Rickert <aidanrickert@comcast.net>
2023-03-11 20:01:10 -0800

    Whatever pneumatics stuff ronan did

--- src/main/java/frc/robot/subsystems/RangeSetter.java
+++ src/main/java/frc/robot/subsystems/RangeSetter.java
@@ -2,17 +2,36 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotContainer;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 // I know nothing. Figure it out Jordan...
 public class RangeSetter extends SubsystemBase {
-    public RangeSetter() {}
+    private DoubleSolenoid sole;
+    private Compressor comp;
+    public RangeSetter(DoubleSolenoid ds, Compressor c) {
+        sole = ds;
+        comp = c;
+    }
  
-    public void initialize() {}
+    public void initialize() {
+        comp.enableDigital();
+        sole.set(DoubleSolenoid.Value.kReverse);
+    }
+
 
     // public Command coneRangeCommand() {
     //     return this.runOnce();
     // }
+    public Command coneRangeCommand() {
+        return this.runOnce(() -> sole.set(DoubleSolenoid.Value.kReverse));
+    }
+
+    public Command cubeRangeCommand() {
+        return this.runOnce(() -> sole.set(DoubleSolenoid.Value.kForward));
+    }
 
 }
