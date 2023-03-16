aad90e29cb0e243f59144c293f480b74b8a86e86
DerpCake1 <kidgamerulez777@gmail.com>
2023-03-15 17:31:53 -0700

    created Crosshair offsets

---
+++ src/main/java/frc/robot/commands/CrosshairOffsets.java
@@ -0,0 +1,30 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.*;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import frc.robot.Constants;
+
+public class CrosshairOffsets extends CommandBase {
+
+    NetworkTable table;
+    NetworkTableEntry tx;
+    NetworkTableEntry ty;
+    NetworkTableEntry ta;
+
+
+    public CrosshairOffsets() {
+
+    }
+
+    @Override
+    public void initialize() {
+        table = NetworkTableInstance.getDefault().getTable("limelight");
+        tx = table.getEntry("tx");
+        ty = table.getEntry("ty");
+        ta = table.getEntry("ta");
+    }
+}
